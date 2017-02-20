dc66717f57b389f3e1744e5c27ff959fcc8a355a
elecabfer <elenam.cabello@gmail.com>
2017-02-20 10:46:06 +0100

    Update Simon.sh

--- Simon.sh
+++ Simon.sh
@@ -1,1 +1,9 @@
- TRIBE_DIR="/scratch/cluster/monthly/ecabello/Simon/TRIBE/CODE/"
+TRIBE_DIR="/scratch/cluster/monthly/ecabello/Simon/TRIBE/CODE/"
+mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A
+
+
+CREATE USER 'username@'localhost' IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON * . * TO 'username'@'localhost';
+FLUSH PRIVILEGES;
+#create mysql database
+CREATE DATABASE mm9seq;
